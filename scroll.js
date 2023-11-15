a93b9775c2545406e257ca825036d2fbfe3c034f
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2023-11-15 18:27:49 +0530

    style: Remove Dull Colors

--- scroll.js
+++ scroll.js
@@ -12,11 +12,6 @@ const backgroundColors = [
     '#00ffff', // Cyan
     '#ff7f00', // Orange
     '#7f00ff', // Purple
-    '#008080', // Teal
-    '#800080', // Purple
-    '#008000', // Dark Green
-    '#800000', // Maroon
-    '#808080', // Gray
   ];
   
   const textColors = [
@@ -31,10 +26,7 @@ const backgroundColors = [
     '#ffffff', // White text on purple background
     '#ffffff', // White text on teal background
     '#ffffff', // White text on purple background
-    '#ffffff', // White text on dark green background
-    '#ffffff', // White text on maroon background
-    '#000000', // Black text on gray background
-  ];
+    ];
   
   // You can use these arrays to pair background and text colors in your styles or applications
   
